7e1aaeeb1de32f4a1dec5721b7cd06728ef3e00a
dapaopaopao <381281999@qq.com>
2019-12-03 18:21:33 +0800

    登录功能

--- src/components/login.vue
+++ src/components/login.vue
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+// import async from 'q'
 export default {
   data () {
     return {
@@ -51,10 +52,15 @@ export default {
       this.$refs.loginRef.resetFields()
     },
     login () {
-      this.$refs.loginRef.validate(valid => {
+      this.$refs.loginRef.validate(async valid => {
         if (!valid) {
           return 0
         }
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
       })
     }
   }
